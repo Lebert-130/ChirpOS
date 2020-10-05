6e441af3de78fee1c0cdb35956d72db6320085df
AestheticalZ <cakeandsweets23@gmail.com>
2020-10-05 02:01:08 +0200

    PC Speaker test

--- kernel.c
+++ kernel.c
@@ -63,6 +63,41 @@ inline void outb(uint16_t port, uint8_t value)
     __asm("outb %b0, %w1"
         :: "a"(value), "d"(port));
 }
+
+static inline uint8_t inb(uint16_t port)
+{
+    uint8_t ret;
+    asm volatile ( "inb %1, %0"
+                   : "=a"(ret)
+                   : "Nd"(port) );
+    return ret;
+}
+
+static void play_sound(uint32_t nFrequence) 
+{
+ 	uint32_t Div;
+ 	uint8_t tmp;
+ 
+    //Set the PIT to the desired frequency
+ 	Div = 1193180 / nFrequence;
+ 	outb(0x43, 0xb6);
+ 	outb(0x42, (uint8_t) (Div) );
+ 	outb(0x42, (uint8_t) (Div >> 8));
+ 
+    //And play the sound using the PC speaker
+ 	tmp = inb(0x61);
+  	if (tmp != (tmp | 3)) 
+	{
+ 		outb(0x61, tmp | 3);
+ 	}
+}
+
+static void nosound() 
+{
+ 	uint8_t tmp = inb(0x61) & 0xFC;
+ 
+ 	outb(0x61, tmp);
+ }
  
 void terminal_initialize(void) 
 {
@@ -162,6 +197,7 @@ void kernel_main(void)
  
 	/* Newline support is left as an exercise. */
 	terminal_writestring("Hello, kernel World!\nTesting tabs\tSee?");
+	play_sound(1000);
 }
 
 int main() 
