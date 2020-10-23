2e74b408d718ca9a058a249b64852219f60ae7e9
Lebert130 <51353708+Lebert130@users.noreply.github.com>
2020-10-23 17:47:03 -0500

    Add files via upload

--- kernel/include/kernel/tty.h
+++ kernel/include/kernel/tty.h
@@ -9,9 +9,14 @@ void terminal_putchar(char c);
 void terminal_write(const char* data, size_t size);
 void terminal_writestring(const char* data);
 void terminal_scroll();
+void terminal_removechar();
 void enable_cursor(uint8_t cursor_start, uint8_t cursor_end);
 void disable_cursor();
 void NMI_enable();
 void NMI_disable();
- 
+void init_pics(int pic1, int pic2);
+
+void reverse(char s[]);
+void change_current_pos();
+
 #endif
