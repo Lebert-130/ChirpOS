caf2355b6b22b20b879bced641f4e7cc5ac2ec3f
Lebert130 <51353708+Lebert130@users.noreply.github.com>
2020-11-22 03:00:09 -0500

    added echo command

--- kernel/kernel/commands.c
+++ kernel/kernel/commands.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <kernel/keyboard_input.h>
 
+int string_size(const char* restrict str)
+{
+    int Size = 0;
+    while (str[Size] != '\0') Size++;
+    return Size;
+}
+
 int send_command(const char* restrict format, ...)
 {
     int sent = 0;
@@ -9,6 +16,24 @@ int send_command(const char* restrict format, ...)
     {
         printf("Hello World!");
         clear_chars(0);
+    }
+
+    if (format[0] == 'e' && format[1] == 'c' && format[2] == 'h' && format[3] == 'o')
+    {
+        char phrase[255];
+
+        for (int i = 5; i < string_size(format); i++)
+        {
+            phrase[i-5] = format[i];
+        }
+
+        printf(phrase);
+
+        for (int i = 0; i < string_size(phrase); i++)
+        {
+            phrase[i] = ' ';
+        }
+        clear_chars(1);
     }
 
     printf("\n>");
